c67d4cbd181672f9cc1f192370da66c7c9bcbb6c
Andria Mae Degoma <degoma.andria@gmail.com>
2023-10-23 18:54:30 +0800

    updated portfolio

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const emailBtn = document.getElementsByClassName('emailBtn');
-const emailText = document.getElementsByClassName('email');
+const emailBtn = document.querySelector('#email-btn');
+const emailText = document.querySelector('.email-text');
 
-emailBtn.addEventListener("click", function(){
+emailBtn.addEventListener("click", () => {
 const textArea = document.createElement('textarea');
 textArea.value = emailText.textContent;
 document.body.appendChild(textArea);
+console.log(emailText.textContent)
 textArea.select();
 document.execCommand("copy");
 document.body.removeChild(textArea);
 emailBtn.setAttribute
 });
-
